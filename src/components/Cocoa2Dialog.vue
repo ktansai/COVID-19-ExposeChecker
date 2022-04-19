01a842b3bcea7a6915d73054bdf55059e35b4307
Keisuke Kawahara <kawahara@whill.jp>
2022-04-19 11:42:13 +0900

    fix dialog sentences

--- src/components/Cocoa2Dialog.vue
+++ src/components/Cocoa2Dialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" width="500">
+  <v-dialog v-model="dialog" width="500" persistent>
     <template v-slot:activator="{ on, attrs }">
       <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
         Click Me
@@ -15,20 +15,22 @@
         <div class="text--primary">
           厚生労働省
           接触確認アプリ(COCOA)のアップデート(2.0.0)に伴い、iOSの設定画面から取得できるログデータの形式が変更になり、
-          COCOA2.0へのアップデート後は当サイト(COCOAログチェッカー)でのログデータの解析が不可能になりました。<br><br>
+          COCOA2.0へのアップデート後は当サイトでのログデータの解析が不可能になりました。<br><br>
           以下の方々は引き続きご利用いただけます。<br>
           <ul>
             <li>Android端末をご利用の方</li>
             <li>iOS端末で、古いバージョンのCOCOAをご利用の方</li>
           </ul>
             <br>
-            iOSユーザーの方に向けては、今後別の提供方法などを模索しています。
-          <br><br>
-          <a
+            iOSユーザーの方に向けては、今後、別の提供方法などを模索しています。<br>
+            再度利用可能になるなど、アップデートありましたら、
+            <a
             target="_blank"
-            href="https://twitter.com/ktansai/status/1512377351950127111"
-            >詳しい説明へ</a
-          >
+            href="https://twitter.com/CocoaLogChecker"
+            >お知らせアカウント(Twitter)</a>
+            でお知らせします。
+          <br><br>
+          
         </div>
       </v-card-text>
 
@@ -36,7 +38,7 @@
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary" text @click="dialog = false"> 閉じる </v-btn>
+        <v-btn color="primary" text @click="dialog = false"> 理解した </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -47,7 +49,7 @@ export default {
   name: "COCOA2Dialog",
   data: function () {
     return {
-      dialog: false,
+      dialog: true,
     };
   },
 };

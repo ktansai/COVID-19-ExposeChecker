97e6d2975eb62eade164a0d8e6ff21dde68b0abe
keisuke KAWAHARA <kawahara@whill.jp>
2022-07-10 19:51:50 +0900

    update v2

--- src/components/ExposeCheckerV2.vue
+++ src/components/ExposeCheckerV2.vue
@@ -3,7 +3,7 @@
       <v-row class="mt-8 justify-center" >
         <div>
         <h1 class="display-2 font-weight-bold my-2">
-          <span class="d-inline-block">COCOAログ</span><span class="d-inline-block">チェッカー2.0(β)</span>
+          <span class="d-inline-block">COCOAログ</span><span class="d-inline-block">チェッカー2.0 (β)</span>
         </h1>
         <p>接触通知ログ解析ツール</p>
 
@@ -13,8 +13,7 @@
       <v-row class="justify-center">
         <div>
         <p class="my-8 mx-2">
-          <span class="d-inline-block">iOS/Androidの設定アプリから閲覧できる</span>
-          <span class="d-inline-block">接触通知のログデータを解析し、</span>
+          <span class="d-inline-block">COCOAからエクスポートできるログデータを解析し、</span>
           <span class="d-inline-block">約2週間の間にCOCOAの新規陽性登録者が付近にいた可能性がある記録を表示します。(Bluetoothの電波が届く範囲)</span>
         </p>
         <p class="my-8 mx-2" style="color:red;" id="beforeUseNote">
@@ -29,36 +28,93 @@
         <v-expansion-panels accordion multiple v-model="panel" >
               <v-expansion-panel id="notes" >
                 <v-expansion-panel-header>詳細説明</v-expansion-panel-header>
-                <v-expansion-panel-content >
-                  現在準備中です。
+                <v-expansion-panel-content class="text-left">
+                  <ul>
+                    <li>新型コロナウイルス接触確認アプリ(COCOA)からエクスポートできるログデータの中で、COCOAの新規陽性登録者の端末の近くにいた記録があるかを確認するツールです。</li><br>
+                    <li>クライアントサイドのJavaScriptで解析を行っているため、ここでペーストしたログデータが外部に送信されるようなことはありません。</li><br>
+                    <li>このログデータで分かることは、COCOAの新規陽性登録者が付近(Bluetoothの電波が届く範囲)にいたことのみで、必ずしも濃厚接触に該当する訳ではありません。</li><br>
+                    <li>BluetoothがOFFに設定されている間は、COCOAが接触検知を行うことができないためご注意ください(<a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou_iryou/covid19_qa_kanrenkigyou_00009.html#Q6-3" target="_blank">厚生労働省FAQ</a>より)</li><br>
+                    <li>本家COCOAのアプリで通知が来る条件は「1m以内かつ15分以上」ですが、本ツールはその基準よりも広い「OSが検知した新規陽性登録者との接触」のログすべてを対象としています。(<a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou_iryou/covid19_qa_kanrenkigyou_00009.html#Q4-7" target="_blank">厚生労働省FAQ</a>より)</li><br>
+                  </ul>
+                    <v-layout justify-center my-4 >
+                    <v-img width="80%" max-width="400px" src="@/assets/images/cocoa-difference.png"/>
+                    </v-layout>
+                  <ul>
+                    <li>本ツールの利用には、新型コロナウイルス接触確認アプリCOCOA(2.0.1以上)のインストールが必要です。COCOAに関しては、<a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/cocoa_00138.html" target="_blank">厚生労働省のHP</a>をご確認ください。</li><br>
+                    <li>本ツールは接触確認アプリの活用事例として厚生労働省HPに掲載いただいております。(<a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/cocoa_00138.html#:~:text=%E3%80%90COCOA%E3%83%AD%E3%82%B0%E3%83%81%E3%82%A7%E3%83%83%E3%82%AB%E3%83%BC%E3%80%91%E3%81%AE%E3%81%94%E5%88%A9%E7%94%A8%E3%81%AF%E3%81%93%E3%81%A1%E3%82%89%E3%81%8B%E3%82%89" target="_blank">厚生労働省のHP</a>)</li><br>
+                    <li>本ツールは無償で使用可能ですが、この解析ツールを用いて起きた問題などについて、製作者は一切の責任を負いかねます。自己責任でご使用ください。</li><br>
+                    <li>不具合報告、改善要望、間違いの指摘、PR、質問などは、<a href="https://github.com/ktansai/COVID-19-ExposeChecker" target="_blank">github</a>、<a href="https://twitter.com/ktansai" target="_blank">@ktansai</a>または、(<a href='mailto:cocoa.log.checker@gmail.com' target="_blank">cocoa.log.checker@gmail.com</a>)までご連絡ください。また、個人で開発しているため、全てに対応できるとは限りません。ご了承ください。</li><br>
+                    <li>COCOAログチェッカー2.0は現在beta版として提供しているため、説明が十分でなかったり、ログ解析に不具合がある場合があります。もし何か疑問点や不具合にお気づきの方はご連絡ください。</li><br>
+                    <li>COCOAログチェッカーは<a href="https://github.com/ktansai/COVID-19-ExposeChecker" target="_blank">github</a>上にソースコードを公開しており、多くの方に提案/指摘をいただきながら作成しています。ご協力いただいている全ての方に多大な感謝を申し上げます。</li>
+                  </ul>
                 </v-expansion-panel-content>
               </v-expansion-panel>
               <v-expansion-panel>
                 <v-expansion-panel-header>使い方(iOS)</v-expansion-panel-header>
                 <v-expansion-panel-content>
-                  現在準備中です。
+                  <p class="text-left">Step1. iOSの設定アプリから接触通知ログをコピーする</p>
+                  <v-img src="@/assets/images/instruction-cocoa2-ios-01.png"/>
+
+                  <br>
+                  <p class="text-left">Step2. 下記入力枠にペーストする</p>
+                  <v-img src="@/assets/images/instruction-ios-02.png"/>
                 </v-expansion-panel-content>
               </v-expansion-panel>
 
               <v-expansion-panel>
                 <v-expansion-panel-header>使い方(Android)</v-expansion-panel-header>
                 <v-expansion-panel-content>
-                  現在準備中です。
+                  <p class="text-left">
+                    Step1. COCOAアプリを開き、[<b>陽性登録者との接触結果を確認</b>]から、[<b>情報を保存</b>]をタップし、GoogleDriveまたはローカルに保存する<br>
+                  </p>
+
+
+                  <p class="text-left text-caption" >接触通知ログは、個人を特定することが難しい秘匿性の高いデータですが、スマホ外部に保存することに抵抗がある方は、ローカルに保存することをおすすめします。</p>
+                  <p style="color:red;">(COCOAアプリのスクリーンショットは後日追加します)</p>
+                  <!-- <v-img src="@/assets/images/instruction-android-01.png" /> -->
+                  <br>
+                  <p class="text-left">
+                    Step2.
+                    GoogleDriveアプリまたは、ローカルのファイルから保存したログデータを開き、
+                    [すべて選択]を押し、[コピー]を押す。<br>
+                  <v-img src="@/assets/images/instruction-android-02.png" />
+                  </p>
+                  <p class="text-left">Step3. 下記入力枠を選択し、[貼り付け]を押す。
+                    <br>
+                    <v-img src="@/assets/images/instruction-android-03.png" />
+                  </p>
                 </v-expansion-panel-content>
             </v-expansion-panel>
             </v-expansion-panels>
         </v-row>
 
-      <v-row class=" justify-center">
-            現在準備中です。
-            <!-- <v-textarea 
+      <v-row >
+
+            <v-textarea 
               v-model=exposeJsonText
               outlined
               label="ここにログファイルをペーストしてください">
-            </v-textarea> -->
+            </v-textarea>
       </v-row>
-      <br>
-
+      <p class="text-caption">
+        <span class="d-inline-block">本ツールは、スマホ内で処理しているため、</span>
+        <span class="d-inline-block">ここでペーストしたデータが外部に流出することはありません。</span>
+      </p>
+      <v-row class="mb-5 justify-center">
+        <v-col cols="6"> 
+          <v-btn
+                v-on:click="clearJson">
+          クリア
+          </v-btn>
+        </v-col>
+        <v-col cols="6"> 
+        <v-btn
+              color="primary"
+              v-on:click="checkJson">
+        チェックする
+        </v-btn>
+        </v-col>
+      </v-row>
       <v-row class="mt-6 mb-6">
         <div v-if="resultText.length > 0">
             <p class="my-5 text-left"> 
@@ -79,6 +135,7 @@
                 </span>
             </p>
         </div>
+        <FAQ id="FAQ"/>
       </v-row>
 
       <v-row class="justify-center mt-16" v-if="resultText.length > 0" >
@@ -129,6 +186,21 @@
         </div>
       </v-row>
       
+      <v-row>
+            <v-textarea 
+              outlined
+              auto-grow
+              readonly
+              label="ここに詳細な結果のデータが出ます"
+              v-bind:value=resultJsonText 
+              class="my-4">
+            </v-textarea>
+      </v-row>
+      <v-col
+        cols="12"
+        class="my-8"
+      >  
+      </v-col>
       <v-row class="mt-8 justify-center">
         <p>made by <a href="https://twitter.com/ktansai">@ktansai</a> / <a href="https://github.com/ktansai/COVID-19-ExposeChecker" target="_blank">github</a></p>
       </v-row>
@@ -138,6 +210,7 @@
 </template>
 
 <script>
+  import FAQ from './FAQ';
 
   function createExposureStatistics(exposureWindows){
     let exposureWindowDict = {}
@@ -165,6 +238,8 @@
   export default {
     name: 'ExposureCheckerV2',
     components: {
+      FAQ,
+      // COCOA2Dialog,
     },
     methods:{
       checkJson: function(){

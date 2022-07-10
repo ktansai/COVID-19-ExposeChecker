33ba42f691eddc5ae4d1792fdc176d140cee57c7
keisuke KAWAHARA <kawahara@whill.jp>
2022-07-10 20:02:43 +0900

    redirect to home

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/ExposeCheckerV2.vue')
+  },
+  {
+    path: '/*',
+    redirect: '/' 
   }
 ]
 
